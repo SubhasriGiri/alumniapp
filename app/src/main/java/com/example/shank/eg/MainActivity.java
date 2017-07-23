2a0d4a9663f2a5e43b3bf4a353a92f202880abeb
shankari giri <shankari giri>
2017-07-23 12:03:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shank/eg/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.shank.eg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
